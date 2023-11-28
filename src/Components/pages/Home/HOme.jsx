d3b482c893e6f5bb25a4892293ba775b8924505a
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2023-11-28 15:15:57 +0600

    showing payment info in employee by table

--- src/Components/pages/Home/HOme.jsx
+++ src/Components/pages/Home/HOme.jsx
@@ -1,5 +1,6 @@
 
 import FAQ from "./FAQ";
+import Industry from "./Industry";
 import Services from "./Services";
 import Slider from "./Slider";
 import Testimonials from "./Testimonials";
@@ -12,6 +13,7 @@ const Home = () => {
             <div className="mx-auto max-w-screen-2xl px-4 py-2 space-y-10 pb-10">
             <Services></Services>
             <Testimonials></Testimonials>
+            <Industry></Industry>
             <FAQ></FAQ>
             </div>
             
