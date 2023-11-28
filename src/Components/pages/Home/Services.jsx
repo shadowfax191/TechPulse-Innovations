973487e41a0a7489a74ef2403fe7c91137717179
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2023-11-28 13:04:18 +0600

    slider and services for home page

--- src/Components/pages/Home/Services.jsx
+++ src/Components/pages/Home/Services.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 const Services = () => {
     return (
         <div>
            <h1 className='text-center text-3xl md:text-5xl font-bold my-4 md:my-8'>Services</h1>
 
            <div>
-            
+
            </div>
         </div>
     );
